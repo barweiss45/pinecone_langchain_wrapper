93ccc6736734ac251cf58247af9979a20d26d24d
Barry Weiss <bweiss@pegasusnet.ai>
2024-02-16 21:03:38 -0500

    update working with namespaces

--- pinecone_connector.py
+++ pinecone_connector.py
@@ -1,13 +1,17 @@
 #! /usr/bin/env python3
 
 import os
-from typing import Any, Dict, NewType, Union
+from typing import Any, Dict, List, NewType, Optional, Union
 
 from dotenv import load_dotenv
-from langchain_pinecone import Pinecone
-from pinecone import IndexDescription, IndexList, DescribeIndexStatsResponse
+from pinecone import DescribeIndexStatsResponse, IndexDescription, IndexList
 from pinecone import Pinecone as PineconeClient
 from pinecone import PodSpec, ServerlessSpec
+
+"""
+Provides management features to Pinecone Vectorstore
+Use Langchain for adding vectors and searches
+"""
 
 serverless = NewType("serverless", str)
 pod = NewType("pod", str)
@@ -16,7 +20,7 @@ load_dotenv()
 
 
 class PineconeConnector(object):
-
+    # Note may not need Singleton
     _instance = None
 
     def __new__(cls, *args, **kwargs):
@@ -24,14 +28,16 @@ class PineconeConnector(object):
             cls._instance = super(PineconeConnector, cls).__new__(cls)
         return cls._instance
 
-    def __init__(self, embeddings):
+    def __init__(self, embeddings, index_names: Optional[List[str]] = None):
         self.embeddings = embeddings
+        self.index_names = index_names
         self.OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
         self.PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-        self.PINECONE_ENV = os.getenv("PINECONE_ENV")
+
         if not hasattr(self, 'pc'):
+            if self.OPENAI_API_KEY is None:
+                raise ValueError("Issue with Pinecone API Key. Value is None.")
             self.pc = PineconeClient(self.PINECONE_API_KEY)
-            self.pinecone_env = self.PINECONE_ENV
 
     def create_index(self,
                      index_name: str,
@@ -39,6 +45,7 @@ class PineconeConnector(object):
                      metric: str = "cosine",
                      server_type: Union[serverless, pod] = "pod",
                      cloud: str = "aws",
+                     region: str = "us-west-2",
                      environment: str = None,
                      pod_type: str = "p1.x1",
                      metadata_config: Dict[str, Any] = {},) -> bool:
@@ -48,15 +55,16 @@ class PineconeConnector(object):
         Args:
             index_name (str): The name of the index to create.
             dimension (int, optional): The dimension of vectors that will be inserted in the index. Defaults to 1536.
-            metric (str, optional): _description_. Defaults to "cosine".
-            cloud (str, optional): _description_. Defaults to "aws".
-            region (str, optional): _description_. Defaults to "None".
+            metric (str, optional): Distance calculation used. Values may be "euclidean",
+                                    "cosine", and "dotproduct." Defaults to "cosine".
+            cloud (str, optional): Used for Serverless instances. Defaults to "aws".
+            region (str, optional): Location for ServerlessSpec. Defaults to "us-west-2". Note: Current us-west-2 is only available region.
+            environment (str, optional): Used in PodSpec same as region is Serverless Spec. Defaults to "None".
+            pod_type (str, optional): Used in PodSpec, Pod type and size used see documentation for specifics. Defaults to "p1.x1"
+            metadata_config (Dict[str, Any]): Metat Data Schema, Defaults to empty dictionary.)
         """
         if index_name in self.pc.list_indexes().names():
             return False
-
-        if self.PINECONE_ENV is not None:
-            environment = self.PINECONE_API_KEY
 
         if server_type == "pod":
             spec = PodSpec(environment,
@@ -64,7 +72,7 @@ class PineconeConnector(object):
                            pods=1,
                            metadata_config=metadata_config)
         elif server_type == "serverless":
-            spec = ServerlessSpec(cloud, region="us-west-2")  # only availabe in us-west-2
+            spec = ServerlessSpec(cloud, region=region)  # only availabe in us-west-2
         else:
             raise ValueError("Incorrect Server type. Choose either ServerlessSpec or PodSpec")
 
@@ -87,9 +95,6 @@ class PineconeConnector(object):
     def list_index(self) -> IndexList:
         """Lists all Pinecone Indexes"""
         return [index for index in self.pc.list_indexes()]
-
-    def add_index():
-        pass
 
     def delete_index(self, index_name: str, time_out: int | None = None):
         try:
